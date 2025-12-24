da7e5702db99e198373926b71f2ecedc033d3726
Yaniv <yanivg1818@gmail.com>
2025-12-24 17:46:58 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from datetime import datetime, timedelta
+
+current_time = datetime.now()
+print("Current time:", current_time.strftime("%H:%M"))
+
+def sleep_cycle(current_time):
+    cycle_length = 90
+    cycles = 1
+    updated_time = current_time
+    for cycle in range(7):
+        wake_up = (updated_time + timedelta(minutes=cycle_length)).strftime("%H:%M")
+        print(f"Wake up {cycles}: {wake_up}")
+        cycles += 1
+        updated_time += timedelta(minutes=cycle_length)
+sleep_cycle(current_time)
+
+
+
+
